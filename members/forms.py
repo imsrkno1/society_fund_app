5196cd5eb323109cb54b483ce7d426010c5b740f
imsrkno1 <68878820+imsrkno1@users.noreply.github.com>
2025-08-01 17:43:03 +0530

    Update forms.py

--- members/forms.py
+++ members/forms.py
@@ -1,24 +1,6 @@
-from django import forms
+# members/forms.py
+from django.contrib.auth.forms import UserCreationForm
 
-# This is the form class for our login page.
-# It defines the two fields we expect from the user:
-# mobile_number and password.
-class LoginForm(forms.Form):
-    # The mobile number field is a CharField, which is a simple text input.
-    mobile_number = forms.CharField(
-        label='Mobile Number',
-        max_length=10,
-        widget=forms.TextInput(attrs={
-            'class': 'form-input w-full',  # Uses your Tailwind CSS class
-            'placeholder': 'Enter your mobile number'
-        })
-    )
-    # The password field is also a CharField, but we use a PasswordInput widget
-    # to hide the text as the user types.
-    password = forms.CharField(
-        label='Password',
-        widget=forms.PasswordInput(attrs={
-            'class': 'form-input w-full', # Uses your Tailwind CSS class
-            'placeholder': 'Enter your password'
-        })
-    )
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields
