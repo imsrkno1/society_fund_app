78dea6892df5b9f718cef724e0c439e182af828f
imsrkno1 <68878820+imsrkno1@users.noreply.github.com>
2025-08-01 18:06:47 +0530

    Update forms.py

--- members/forms.py
+++ members/forms.py
@@ -1,6 +1,11 @@
 # members/forms.py
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+# This is a basic custom form for user creation.
+# It inherits from Django's built-in UserCreationForm.
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
+        model = User
         fields = UserCreationForm.Meta.fields
